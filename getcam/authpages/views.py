78db47e1d4f57011419210fd6e0747da7c7ebbcd
chaitanya <kc508275@gmail.com>
2020-12-04 23:13:57 +0530

    1.1 v

--- getcam/authpages/views.py
+++ getcam/authpages/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-
+from .models import  Profile
 # Create your views here.
 from django.contrib.auth import authenticate,login
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import HttpResponse
-from .forms import loginform
+from .forms import loginform,UserRegistrationForm
 @login_required
 def dashboard(request):
     return render(request,'auth/dashboard.html',{'section':'dashboard'})
@@ -26,3 +26,20 @@ def userlogin(request):
     else:
         form = loginform()
     return render(request,'auth/login.html',{'form':form})
+
+
+def register(request):
+        if request.method == 'POST':
+            user_form = UserRegistrationForm(request.POST)
+            if user_form.is_valid():
+                new_user = user_form.save(commit = False)
+                new_user.set_password(user_form.cleaned_data['password'])
+                new_user.save()
+                Profile.objects.create(user = new_user)
+                return render(request,'auth/register_done.html',{'new_user':new_user})
+        else:
+            user_form = UserRegistrationForm()
+            
+        # return render(request,'auth/register_done.html')
+            return render(request,'auth/register.html',{'user_form' : user_form})
+        
